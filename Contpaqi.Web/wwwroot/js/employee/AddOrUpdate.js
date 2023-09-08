feb05a440bd2ddcceb08010eaf65ad341a2b8312
Emiliano Elicegui <emielicegui@gmail.com>
2023-09-08 15:52:18 -0300

    add libs

---
+++ Contpaqi.Web/wwwroot/js/employee/AddOrUpdate.js
@@ -0,0 +1,63 @@
+﻿$(document).ready(function () {
+    const form = document.getElementById("employeeForm");
+
+    var url = window.location.pathname; 
+    var segments = url.split('/'); 
+    var employeeId = segments[segments.length - 1]; 
+
+    if (!isNaN(employeeId)) {
+        console.log(employeeId);
+        axios.get(apiUrl + "/employee/Get/" + employeeId) // Ajusta la URL según tu enrutamiento
+            .then(function (response) {
+                if (response.status === 200) {
+                    var employee = response.data;
+                    populateEmployeeForm(employee);
+                } else {
+                    console.error("Error al obtener los datos del empleado.");
+                }
+            })
+            .catch(function (error) {
+                console.error("Error en la solicitud Axios: " + error);
+                alert(error);
+            });
+    } else {
+        console.error("ID de empleado no válido en la URL.");
+    }
+
+    function populateEmployeeForm(employee) {
+        $("#Id").val(employee.id);
+        $("#Name").val(employee.name);
+        $("#LastName").val(employee.lastName);
+        $("#MiddleName").val(employee.middleName);
+        $("#Address").val(employee.address);
+        $("#PhoneNumber").val(employee.phoneNumber);
+        $("#Age").val(employee.age);
+        $("#CivilStatus").val(employee.civilStatus);
+        $("#DateOfBirth").val(employee.dateOfBirth.substring(0, 10));
+        $("#Position").val(employee.position);
+        $("#Email").val(employee.email);
+        $("#Gender").val(employee.gender);
+        $("#Rfc").val(employee.rfc);
+        $("#EndDate").val(employee.endDate);
+    }
+   
+    $("#submitButton").click(function () {
+        const formData = new FormData(form);
+        const data = {};
+        formData.forEach((value, key) => {
+            data[key] = value;
+        });
+
+        if (data["EndDate"] === "") {
+            data["EndDate"] = null; 
+        }
+
+        axios.put(apiUrl + "/employee/AddOrUpdate", data)
+            .then(response => {
+                window.location.href = '/Employee/Index';
+            })
+            .catch(error => {
+                alert("Error al guardar el empleado.");
+            });
+    });
+});
---
+++ Contpaqi.Web/wwwroot/js/employee/employeeDatatable.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+    var dataTable = $("#employeeTable").DataTable({
+        "processing": true,
+        "serverSide": true,
+        "filter": false,
+        "ajax": {
+            "url": apiUrl + "/employee/GetAll",
+            "type": "POST",
+            "datatype": "json",
+            "contentType": "application/json", 
+            "data": function (data) {
+                data.Name = $("#NameFilter").val();
+                data.Rfc = $("#RfcFilter").val();
+                data.Status = $("#StatusFilter").val();
+                return JSON.stringify(data);
+            }
+        },
+        "columns": [
+            { "data": "fullName", "name": "Nombre Completo", "autoWidth": true },
+            { "data": "email", "name": "Email", "autoWidth": true },
+            { "data": "position", "name": "Posición", "autoWidth": true },
+            { "data": "rfc", "name": "Rfc", "autoWidth": true },
+            { "data": "hireDate", "name": "Fecha Baja", "autoWidth": true },
+            {
+                "data": null,
+                "render": function (data, type, row) {
+                    return '<button class="btn btn-primary btn-edit" data-id="' + data.id + '">Editar</button>';
+                },
+                "autoWidth": true
+            }
+        ],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json"
+        },
+        "error": function (xhr, errorText) {
+            console.log("Error en la solicitud AJAX: " + errorText);
+        }
+    });
+
+    $('#employeeTable tbody').on('click', '.btn-edit', function () {
+        var employeeId = $(this).data('id');
+        window.location.href = '/Employee/AddOrUpdate/' + employeeId;
+    });
+
+    $("#btnFilter").on("click", function () {
+        dataTable.ajax.reload();
+    });
+
+    $("#btnClear").on("click", function () {
+        $("#NameFilter").val(""); 
+        $("#RfcFilter").val("");
+        $("#StatusFilter").val(0); 
+        dataTable.ajax.reload(); 
+    });
+});
